77ad500c21ae97299ee12830ff3f9b4ed2ec73c2
Tim O'Donnell <tim@cs.brown.edu>
2007-04-19 03:42:49 +0000

    more simulator dev

---
+++ code/simulator/Statement.C
@@ -0,0 +1,14 @@
+#include "Statement.H"
+#include "StatementInfo.H"
+
+Statement::Statement(void) {}
+
+static QHash<QString, StatementInfo>* Statement::getStatementInfoHash(void) {
+	
+	if (s_statementInfoHash == NULL) {
+		s_statementInfoHash = new QHash<QString, StatementInfo>();
+		
+		//s_statementInfoHash[Add::s_prefix] =
+	}
+	return s_statementInfoHash;
+}
---
+++ code/simulator/main.C
@@ -0,0 +1,10 @@
+#include "State.H"
+#include "Statement.H"
+#include "StatementImplementations.H"
+#include "ParseList.H"
+#include "ParseNode.H"
+
+int main(int argc, char** argv) {
+	
+	return 0;	
+}
